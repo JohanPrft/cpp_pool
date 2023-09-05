43cf21be1eda33a3988c589f6b6a22fe69be15ab
jprofit <jojoman69100@gmail.com>
2023-09-05 17:07:09 +0200

    add cpp09/02

--- cpp09/ex02/includes/PmergeMe.hpp
+++ cpp09/ex02/includes/PmergeMe.hpp
@@ -3,8 +3,25 @@
 
 #include <iostream>
 #include <algorithm>
+#include <limits>
+#include <string>
+#include <cerrno>
 #include <vector>
-#include <list>
+#include <deque>
+
+/*
+ *	Merge-Insertion Sort : Ford–Johnson algorithm is a comparison sorting algorithm published in 1959.
+ *
+ *	Input X: n elements.
+ *	Group the elements of X into n/2 pairs, if odd leave the last one unpaired.
+ *	Sort the pairs and put to the right the larger. (merge sort part)
+ *	Recursively sort the pairs by their largest element, creating a sequence S of n/2 number in ascending order.
+ *	Insert at the start of S the element paired with the smallest number in S
+ *	Insert the remaining n/2 - 1 elements (insertion sort part)
+ *
+ *	The algorithm is designed to take advantage that the binary searches used to insert elements into S are most efficient
+ *	when the length of the subsequence that is searched is one less than a power of two.
+ */
 
 class PmergeMe {
 
@@ -14,11 +31,85 @@ private:
 	PmergeMe &operator=(const PmergeMe &cpy);
 	~PmergeMe();
 
-	std::vector<int> _vector;
-
-
 public:
-	static void mergeInsertSort(std::vector<int>& array, int left, int right);
+	// algorithm based on divide and conquer strategy
+	template <typename T>
+	static void merge(T& array, int left, int mid, int right)
+	{
+		int subVectorOneSize = mid - left + 1;
+		int subVectorTwoSize = right - mid;
+
+		T leftVector(subVectorOneSize);
+		T rightVector(subVectorTwoSize);
+
+		for (int i = 0; i < subVectorOneSize; i++)
+			leftVector[i] = array[left + i];
+
+		for (int j = 0; j < subVectorTwoSize; j++)
+			rightVector[j] = array[mid + 1 + j];
+
+		int i = 0, j = 0, k = left;
+		while (i < subVectorOneSize && j < subVectorTwoSize)
+		{
+			if (leftVector[i] <= rightVector[j])
+			{
+				array[k] = leftVector[i];
+				i++;
+			} else
+			{
+				array[k] = rightVector[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < subVectorOneSize)
+		{
+			array[k] = leftVector[i];
+			i++;
+			k++;
+		}
+
+		while (j < subVectorTwoSize)
+		{
+			array[k] = rightVector[j];
+			j++;
+			k++;
+		}
+	}
+
+	// algorithm growing a sorted list in front
+	// best for little list
+	template <typename T>
+	static void insertionSort(T& arr, int left, int right)
+	{
+		for (int i = left + 1; i <= right; i++)
+		{
+			int key = arr[i];
+			int j = i - 1;
+			while (j >= left && arr[j] > key)
+			{
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	template <typename T>
+	static void mergeInsertSort(T& array, int left, int right)
+	{
+		if (right - left + 1 <= 2)
+		{
+			insertionSort(array, left, right);
+		} else if (left < right)
+		{
+			int mid = left + (right - left) / 2;
+			mergeInsertSort(array, left, mid);
+			mergeInsertSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
 };
 
 
--- cpp09/ex02/sources/main.cpp
+++ cpp09/ex02/sources/main.cpp
@@ -1,30 +1,71 @@
 #include "../includes/PmergeMe.hpp"
 
-void printVector(const std::vector<int>& vec) {
-	for (std::vector<int>::const_iterator num = vec.begin(); num != vec.end(); num++) {
+int INT_MIN = std::numeric_limits<int>::min();
+int INT_MAX = std::numeric_limits<int>::max();
+
+template<typename T>
+void printVector(const T& vec) {
+	for (typename T::const_iterator num = vec.begin(); num != vec.end(); num++) {
 		std::cout << *num << " ";
 	}
 	std::cout << std::endl;
 }
 
+std::vector<int> createVector(char **argv)
+{
+	std::vector<int> vector;
+	long i = 1;
+	while (argv[i])
+	{
+		errno = 0;
+		long n = std::strtol(argv[i], NULL, 10);
+		if (errno == ERANGE || n < INT_MIN || n > INT_MAX)
+		{
+			std::cout << "Error: " << n << " is not an int" << std::endl;
+			++i;
+			continue;
+		}
+		vector.push_back(static_cast<int>(n));
+		++i;
+	}
+	return (vector);
+}
+
+std::deque<int> createDeque(char **argv)
+{
+	std::deque<int> vector;
+	long i = 1;
+	while (argv[i])
+	{
+		errno = 0;
+		long n = std::strtol(argv[i], NULL, 10);
+		if (errno == ERANGE || n < INT_MIN || n > INT_MAX)
+		{
+			std::cout << "Error: " << n << " is not an int" << std::endl;
+			++i;
+			continue;
+		}
+		vector.push_back(static_cast<int>(n));
+		++i;
+	}
+	return (vector);
+}
+
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-
-	std::vector<int> v;
-	v.push_back(5);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(1);
-	v.push_back(7);
-	v.push_back(6);
-	v.push_back(4);
-
-	std::cout << "Given vector is:\n";
-	printVector(v);
-	PmergeMe::mergeInsertSort(v, 0, v.size() - 1);
-	std::cout << "\nSorted vector is:\n";
-	printVector(v);
+	if (argc < 2)
+	{
+		std::cout << "Usage: ./PmergeMe 1 2 3 [...]" << std::endl;
+		return 1;
+	}
+	std::vector<int> vec = createVector(argv);
+	std::deque<int> vec1 = createDeque(argv);
+	printVector(vec);
+	printVector(vec1);
+	PmergeMe::mergeInsertSort(vec, 0, vec.size() - 1);
+	PmergeMe::mergeInsertSort(vec1, 0, vec1.size() - 1);
+	printVector(vec);
+	printVector(vec1);
 	return 0;
 }
+
